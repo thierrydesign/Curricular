179c7bfa593ff97448bdcf62481a9021bf44340f
thierrydesign <thierry.guedes.89@gmail.com>
2023-11-20 17:39:39 -0300

    Projeto Curricular

---
+++ src/main/java/com/curricular/CurricularApplication.java
@@ -0,0 +1,13 @@
+package com.curricular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurricularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurricularApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/curricular/CurricularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curricular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurricularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
